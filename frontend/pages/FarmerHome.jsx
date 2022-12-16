86a3cf4acb198a04bd94be8d2edaae65bf0a18ea
Ezra <gerkibz@outlook.com>
2022-12-16 16:48:03 +0300

    add farmer homepage

--- frontend/pages/FarmerHome.jsx
+++ frontend/pages/FarmerHome.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import SideNav from "../components/SideNav";
 import AddProduceForms from "./Farmer/AddProduceForms";
+import Header from "./Farmer/Header";
 
 function FarmerHome() {
   return (
-    <div class="bg-white grid grid-rows-1 ">
+    <div className="bg-white min-h-screen">
       {/* sidenav -> produce list, add produce form, allow list table, send confirmation button, closed contracts table*/}
 
-      <AddProduceForms />
+      <Header />
+      <div className="mt-3 flex justify-center items-center w-1/2">
+        <AddProduceForms />
+      </div>
 
       {/* produce list */}
       {/* add produce form */}
