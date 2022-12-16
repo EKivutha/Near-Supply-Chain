454a2ed6c213f8c6bf08f8ccbcd0971e9302ee16
Ezra <gerkibz@outlook.com>
2022-12-16 16:56:37 +0300

    add allow list form

--- frontend/pages/Investor/AllowListForm.jsx
+++ frontend/pages/Investor/AllowListForm.jsx
@@ -23,20 +23,64 @@ function AllowListForm() {
   }
   return (
     <section>
-      <form onSubmit={changeGreeting} className="change">
-        <label>Update Produce Name</label>
-        <div>
-          <input
-            autoComplete="off"
-            defaultValue={valueFromBlockchain}
-            id="greetingInput"
-          />
-          <button>
-            <span>Save</span>
-            <div className="loader"></div>
-          </button>
-        </div>
-      </form>
+      <div class="w-full max-w-xs">
+        <form
+          onSubmit={changeGreeting}
+          class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        >
+          <div class="mb-4">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2"
+              for="username"
+            >
+              Crop
+            </label>
+            <input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Maize"
+            />
+          </div>
+          <div class="mb-6">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2"
+              for="password"
+            >
+              Crop Variety
+            </label>
+            <input
+              class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="Pannar 691 Maize Variety"
+            />
+          </div>
+          <div class="mb-6">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2"
+              for="password"
+            >
+              Price/KG
+            </label>
+            <input
+              class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="KSH60"
+            />
+          </div>
+          <div class="flex items-center justify-between">
+            <button
+              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Save
+              <div className="loader"></div>
+            </button>
+          </div>
+        </form>
+      </div>
     </section>
   );
 }
